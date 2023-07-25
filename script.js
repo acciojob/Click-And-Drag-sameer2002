1c6ebb5756890d259a78f8ba0a82c79e78a0c95f
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-26 01:56:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your code here.
+ const items = document.querySelector('.items');
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+ 
+        items.addEventListener('mousedown', (e) => {
+            isDown = true;
+           startX = e.pageX - items.offsetLeft;
+           scrollLeft = items.scrollLeft;
+        });
+ 
+        items.addEventListener('mouseleave', () => {
+            isDown = false;
+        });
+ 
+        items.addEventListener('mouseup', () => {
+            isDown = false;
+        });
+ 
+        items.addEventListener('mousemove', (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - items.offsetLeft;
+           const walk = (x - startX) * 2;
+            items.scrollLeft = scrollLeft - walk;
+        });
